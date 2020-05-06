baa625352dfc4965cced65277c3556df0a3baaef
Bram Reuling <bram.reuling@outlook.com>
2020-05-06 14:01:48 +0200

    Trying to fix collisions

--- Objects/Player.cs
+++ Objects/Player.cs
@@ -1,104 +1,161 @@
 ﻿using System;
 using GXPEngine;
+using GXPEngine.Core;
 
 public class Player : AnimationSprite
 {
 
-	private int _moveSpeed { get; set; }
+    private int _moveSpeed { get; set; }
+    private int _moveSpeedY { get; set; }
+    private int _animationDrawsBetweenFrames { get; set; }
+    private int _step { get; set; }
 
-	Vec2 _position { get; set; }
-	Vec2 _velocity;
+    Vec2 _position;
+    Vec2 _oldPosition { get; set; }
+    Vec2 _velocity;
 
-	public Player() : base("barry.png", 7 , 1)
-	{
-		SetOrigin(width / 2, height / 2);
-		SetXY(game.width / 2, game.height / 2);
-		_position = new Vec2(game.width / 2, game.height / 2);
-		_moveSpeed = 5;
-	}
 
-	//------------------------------------------------------------------------------------------------------------------------
-	//														Update()
-	//------------------------------------------------------------------------------------------------------------------------
-	void Update()
-	{
-		movement();
-		updatePos();
-		updateScreenPos();
-	}
+    public Player(float x, float y) : base("barry.png", 7, 1)
+    {
+        SetOrigin(width / 2, height / 2);
+        SetXY(x, y);
+        _position = new Vec2(x, y);
 
-	//------------------------------------------------------------------------------------------------------------------------
-	//														updatePos()
-	//------------------------------------------------------------------------------------------------------------------------
-	private void updatePos()
-	{
-		x = _position.x;
-		y = _position.y;
-	}
+        _moveSpeed = 5;
+        _moveSpeedY = _moveSpeed;
 
-	//------------------------------------------------------------------------------------------------------------------------
-	//														updateScreenPos()
-	//------------------------------------------------------------------------------------------------------------------------
-	private void updateScreenPos()
-	{
-		// Euler integration
-		_position += _velocity;
-	}
+        _animationDrawsBetweenFrames = 32;
+        _step = 0;
 
-	//------------------------------------------------------------------------------------------------------------------------
-	//														movement()
-	//------------------------------------------------------------------------------------------------------------------------
-	private void movement()
-	{
-		// UP
-		if (Input.GetKey(Key.W))
-		{
-			_velocity.y = -_moveSpeed;
-		}
+    }
 
-		// DOWN
-		if (Input.GetKey(Key.S))
-		{
-			_velocity.y = _moveSpeed;
-		}
+    //------------------------------------------------------------------------------------------------------------------------
+    //														Update()
+    //------------------------------------------------------------------------------------------------------------------------
+    void Update()
+    {
 
-		// RIGHT
-		if (Input.GetKey(Key.D))
-		{
-			_velocity.x = _moveSpeed;
-		}
+        _oldPosition = _position;
 
-		// LEFT
-		if (Input.GetKey(Key.A))
-		{
-			_velocity.x = -_moveSpeed;
-		}
+        movement();
+        updatePos();
+        updateScreenPos();
+    }
 
-		noInputCheck();
+    //------------------------------------------------------------------------------------------------------------------------
+    //														updatePos()
+    //------------------------------------------------------------------------------------------------------------------------
+    private void updatePos()
+    {
+        x = _position.x;
+        y = _position.y;
+    }
 
-		normalizeVelocity();
-	}
+    //------------------------------------------------------------------------------------------------------------------------
+    //														updateScreenPos()
+    //------------------------------------------------------------------------------------------------------------------------
+    private void updateScreenPos()
+    {
+        // Euler integration
+        _position += _velocity;
+    }
 
-	//------------------------------------------------------------------------------------------------------------------------
-	//														normalizeVelocity()
-	//------------------------------------------------------------------------------------------------------------------------
-	private void normalizeVelocity()
-	{
-		// Normalize the velocity to have the same movement on diagonal as non-diagonal
-		_velocity.Normalize();
-		_velocity *= _moveSpeed;
-	}
+    //------------------------------------------------------------------------------------------------------------------------
+    //														movement()
+    //------------------------------------------------------------------------------------------------------------------------
+    private void movement()
+    {
+        // UP
+        if (Input.GetKey(Key.W))
+        {
+            _velocity.y = -_moveSpeedY;
+        }
 
-	//------------------------------------------------------------------------------------------------------------------------
-	//														noInputCheck()
-	//------------------------------------------------------------------------------------------------------------------------
-	private void noInputCheck()
-	{
-		// If the input is nothing than don't move
-		if (!Input.GetKey(Key.W) && !Input.GetKey(Key.S) && !Input.GetKey(Key.D) && !Input.GetKey(Key.A))
-		{
-			_velocity.x = 0;
-			_velocity.y = 0;
-		}
-	}
+        // DOWN
+        if (Input.GetKey(Key.S))
+        {
+
+            _velocity.y = _moveSpeedY;
+
+        }
+
+        // RIGHT
+        if (Input.GetKey(Key.D))
+        {
+
+            _velocity.x = _moveSpeed;
+            scaleX = 1;
+
+        }
+
+        // LEFT
+        if (Input.GetKey(Key.A))
+        {
+
+            _velocity.x = -_moveSpeed;
+            scaleX = -1;
+        }
+
+        noInputCheck();
+
+        normalizeVelocity();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------------
+    //														normalizeVelocity()
+    //------------------------------------------------------------------------------------------------------------------------
+    private void normalizeVelocity()
+    {
+        // Normalize the velocity to have the same movement on diagonal as non-diagonal
+        _velocity.Normalize();
+        _velocity *= _moveSpeed;
+    }
+
+    //------------------------------------------------------------------------------------------------------------------------
+    //														noInputCheck()
+    //------------------------------------------------------------------------------------------------------------------------
+    private void noInputCheck()
+    {
+        // If the input is nothing than don't move
+        if (!Input.GetKey(Key.W) && !Input.GetKey(Key.S) && !Input.GetKey(Key.D) && !Input.GetKey(Key.A))
+        {
+            _velocity.x = 0;
+            _velocity.y = 0;
+        }
+
+    }
+
+    void OnCollision(GameObject _other)
+    {
+        if (_other is CollisionTile)
+        {
+            //Collision collWithTile = collider.GetCollisionInfo(_other.collider);
+            CollisionTile collTile = _other as CollisionTile;
+
+            if (x + width + _velocity.x * _moveSpeed <= collTile.x + _moveSpeed)
+            {
+                //Console.WriteLine("TOP");
+                //Console.WriteLine(collWithTile.normal.y);
+                //_moveSpeedY = 0;
+                _velocity.x = 0;
+                Console.WriteLine("HIT");
+            }
+            //else if (collWithTile.normal.x < 0)
+            //{
+            //    //Console.WriteLine("RIGHT");
+            //    //Console.WriteLine(collWithTile.normal.x);
+            //    //_velocity.y = 0;
+
+            //}
+            //else if (collWithTile.normal.y > 0)
+            //{
+            //    //_moveSpeedY = 0;
+
+            //}
+            //else if (collWithTile.normal.x > 0)
+            //{
+            //    //_velocity.x = 0;
+            //}
+        }
+    }
 }
