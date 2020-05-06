7a6ce82ea2e8e3c2bdf14c274d4b246f45b43475
Bram Reuling <bram.reuling@outlook.com>
2020-05-06 15:57:15 +0200

    Added Background Tiles

---
+++ Other/BackgroundTile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class BackgroundTile : AnimationSprite
+    {
+        public BackgroundTile(string filename, int cols, int rows) : base(filename, cols, rows)
+        {
+            SetOrigin(width / 2, height / 2);
+        }
+    }
+}
