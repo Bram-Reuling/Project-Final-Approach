47f41a501dbe2cf4f1ba214fd814a84604234b2e
Bram Reuling <bram.reuling@outlook.com>
2020-05-11 20:22:27 +0200

    Added the multiplayer room

--- Rooms/BarHub.cs
+++ Rooms/BarHub.cs
@@ -6,14 +6,15 @@ class BarHub : Hub
 	private string _fileName { get; set; }
 	public bool _firstInstance { get; set; }
 
-	public BarHub(MainGame _game) : base("checkers.png", 1, 1, "square.png", 1, 1, "colors.png", 1, 1, _game)
+	public BarHub(MainGame _game) : base("Sprites/Bar.png", 32, 24, "Sprites/Bar.png", 32, 24, "colors.png", 1, 1, _game)
 	{
 		_fileName = @"Screens/BarHub/BarHub.tmx";
 
 		Map _levelData = MapParser.ReadMap(_fileName);
-		SpawnCollisionTiles(_levelData);
 		SpawnBackgroundTiles(_levelData);
+		SpawnCollisionTiles(_levelData);
 		SpawnObjects(_levelData);
+		SpawnOverlapTiles(_levelData);
 
 		_firstInstance = false;
 	}
