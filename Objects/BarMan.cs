1a6df5ea3765910b68d0c01f58e87cd82e06d623
Bram Reuling <bram.reuling@outlook.com>
2020-05-13 16:39:13 +0200

    Implemented the Barman and DJ NPC's

--- Objects/BarMan.cs
+++ Objects/BarMan.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using GXPEngine;
 
-namespace GXPEngine.Objects
+class BarMan : AnimationSprite
 {
-    class BarMan
-    {
-    }
+
+	int animationDrawsBetweenFrames;
+	int step;
+
+	public BarMan() : base("Sprites/Employee.png", 6, 2)
+	{
+		SetOrigin(width / 2, height / 2);
+		SetXY(game.width / 2 + 415, game.height / 2 - 50);
+		scale = 0.15f;
+
+		animationDrawsBetweenFrames = 6;
+		step = 0;
+	}
+
+	public void Update()
+	{
+		step = step + 1;
+		if (step > animationDrawsBetweenFrames)
+		{
+			if (currentFrame > 5 && currentFrame < 9)
+			{
+				NextFrame();
+				step = 0;
+			}
+			else
+			{
+				SetFrame(6);
+			}
+		}
+	}
 }
--- Objects/DJ.cs
+++ Objects/DJ.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using GXPEngine;
 
-namespace GXPEngine.Objects
+class DJ : AnimationSprite
 {
-    class DJ
-    {
-    }
+
+	int animationDrawsBetweenFrames;
+	int step;
+
+	public DJ() : base("Sprites/Employee.png", 6, 2)
+	{
+		SetOrigin(width / 2, height / 2);
+		SetXY(game.width / 2 - 320, game.height / 2 - 200);
+		scale = 0.25f;
+
+		animationDrawsBetweenFrames = 6;
+		step = 0;
+	}
+
+	public void Update()
+	{
+		step = step + 1;
+		if (step > animationDrawsBetweenFrames)
+		{
+			if (currentFrame < 5)
+			{
+				NextFrame();
+				step = 0;
+			}
+			else
+			{
+				SetFrame(0);
+			}
+		}
+	}
 }
